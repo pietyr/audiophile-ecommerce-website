0a3e614b95deb19f28c50e33362ac253f608b8f1
Piotr Kisiel <piotr.kisiel.6@gmail.com>
2024-12-12 22:23:21 +0100

    feat: clear App.vue and add CategoryView

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import CategoryView from '@/views/CategoryView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,6 +11,11 @@ const router = createRouter({
       component: HomeView,
     },
     {
+      path: '/category/:name',
+      name: 'category',
+      component: CategoryView,
+    },
+    {
       path: '/about',
       name: 'about',
       // route level code-splitting
@@ -18,6 +24,6 @@ const router = createRouter({
       component: () => import('../views/AboutView.vue'),
     },
   ],
-})
+});
 
-export default router
+export default router;
